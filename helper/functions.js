7c6602f825af32eb9c418b33924fffea2fbe8ee3
AndriusDulevicius <andriusdulevicius@email.com>
2021-05-28 13:43:37 +0300

    handleFindErr function added, delete todo route started

--- helper/functions.js
+++ helper/functions.js
@@ -4,6 +4,14 @@ function findTodoWithId(id) {
   return todoDb.find((t) => t.id === +id);
 }
 
+function handleFindErr(paramId, res) {
+  console.log('todo not found');
+
+  //http status codes galima pasigooglinti
+  return res.status(404).json({ error: `todo with id ${paramId} does not exist` });
+}
+
 module.exports = {
   findTodoWithId,
+  handleFindErr,
 };
--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 //visa info npm express , kad paziureti nuo ko pradeti , ir kaip atrodo visas express remas
 const express = require('express');
-const { findTodoWithId } = require('./helper/functions');
+const { findTodoWithId, handleFindErr } = require('./helper/functions');
 const todoDb = require('./DB/todoDb');
 
 const app = express();
 
+//get all todos
 app.get('/api/todos', (req, res) => {
   res.json(todoDb);
 });
-
+//get one todo
 app.get('/api/todos/:id', (req, res) => {
   const found = findTodoWithId(req.params.id);
+  //req.params.id - bus nurodytas todo linke :id vietoje
+  if (!found) {
+    handleFindErr(req.params.id, res);
+    return;
+  }
 
-  res.json({ result: found });
+  res.json(found);
+});
+//delete one todo
+
+app.delete('/api/todos/:id', (req, res) => {
+  const paramId = req.params.id;
+  const found = findTodoWithId(paramId);
+
+  if (!found) {
+    handleFindErr(paramId, res);
+    return;
+  }
+
+  res.json({ youWantToDelete: found });
 });
 
 app.listen(3000, () => console.log('server is runing'));
